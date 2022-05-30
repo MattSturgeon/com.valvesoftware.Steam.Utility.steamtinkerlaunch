f7958b29d819fd8953b89abc29026f8cd78c08bb
HanFox <mafriki@gmail.com>
2022-05-30 12:18:46 +0100

    Current state of the project

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # com.valvesoftware.Steam.Utility.steamtinkerlaunch
-Tests and fiddles
+
+Testing repo ahead of the tool being accepted on Flathub (maybe longer).
+
+Will likely end up tracking certain Steam Tinker Launch commits instead of "stable" releases.
+
+# Reporting issues
+
+Currently all Steam Tinker Launch flatpak discussion is happening here: https://github.com/frostworx/steamtinkerlaunch/issues/27
+
+# Want to test it?
+
+    1. Install the [Steam Flatpak](https://beta.flathub.org/apps/details/com.valvesoftware.Steam) if you haven't. Unfortunately, this will only work with the Steam Flatpak.
+
+    2. Open the terminal.
+
+    3. Run `git clone https://github.com/HanPrower/com.valvesoftware.Steam.Utility.steamtinkerlaunch` in the terminal.
+
+    4. Run `chmod +x fp-build.sh`. This is a file that just automates some of the fiddle flatpak-builder bits. Feel free to check the file first.
+
+    5. Run `./fp-build.sh com.valvesoftware.Steam.Utility.steamtinkerlaunch.yml`. You can safely ignore the Appstream check error. If the utility fails to install, you likely don't have Flathub added for your user specifically. Run `flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo` if that is the case.
+
+    6. Run `flatpak list --user | grep steamtinkerlaunch` to double check that it is installed.
+
+    7. Open the Steam Flatpak. You can run it from the application menu or by running `flatpak run com.valvesoftware.Steam` in the terminal.
+
+    8. Right click the game you want to test, then click "Properties". Afterwards select "Compatibilty" and check "Force the use of a specific compatibility tool" and select "Steam Tinker Launch".
+
+    9. Launch the game and you should see the steamtinkerlaunch menu. Press "MAIN MENU" to look at the menu.
+
+# Sick of testing it?
+
+If you want to revert everything, do the following:
+
+    1. Run `flatpak remove com.valvesoftware.Steam.Utility.steamtinkerlaunch`.
+
+    2. Click "Properties" on any game you enabled Steam Tinker Launch for, select "Compatibilty", and uncheck "Force the use of a specific compatibility tool".
+
+    3. Run `flatpak remote-delete --user flathub`. This will remove the Flathub remote from the user.
---
+++ fp-build.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env sh
+#
+# Copyright © 2021 Lionir
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation;
+# version 2.1 of the License.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see <https://www.gnu.org/licenses/>.
+#
+
+if ! [ -f "$1" ]
+then
+    echo "$1 doesn't exist" >&2
+    exit
+fi
+
+XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+
+if ! flatpak-builder \
+    --download-only --no-shallow-clone \
+    --force-clean --allow-missing-runtimes \
+    --ccache \
+    --state-dir="$XDG_CACHE_HOME/flatpak-builder" \
+    "$XDG_CACHE_HOME/flatpak-builder-builddir/${1%.*}" "$1"
+then
+    echo "Download failed!" >&2
+    exit 1
+fi
+
+if ! flatpak-builder \
+    --verbose --sandbox --user \
+    --bundle-sources --force-clean --ccache \
+    --install-deps-from=flathub \
+    --default-branch=test \
+    --state-dir="$XDG_CACHE_HOME/flatpak-builder" \
+    --extra-sources="$XDG_CACHE_HOME/flatpak-builder/downloads" \
+    "$XDG_CACHE_HOME/flatpak-builder-builddir/${1%.*}" "$1"
+then
+    echo "Build failed!" >&2
+    exit 1
+fi
+
+if ! flatpak-builder \
+    --user --install --force-clean \
+    --repo="$XDG_CACHE_HOME/flatpak-builder-repo/" \
+    --default-branch=test \
+    --state-dir="$XDG_CACHE_HOME/flatpak-builder" \
+    "$XDG_CACHE_HOME/flatpak-builder-builddir/${1%.*}" "$1"
+then
+    echo "Committing or install failed" >&2
+    exit 1
+fi
+
+flathub_json=$(dirname "$1")/flathub.json
+
+if zgrep -q "<id>${1%.*}\(\.\w\+\)*\(.desktop\)\?</id>" "$XDG_CACHE_HOME/flatpak-builder-builddir/${1%.*}/files/share/app-info/xmls/${1%.*}.xml.gz"
+then
+    echo "---"
+    echo "AppID check.. passed!"
+else
+    echo "---"
+    echo "AppID check.. failed!" >&2
+fi
+
+if [ -e "$flathub_json" ] && python3 -c 'import sys, json; sys.exit(not json.load(sys.stdin).get("skip-icons-check", False))' < "$flathub_json"
+then
+    echo "Skipping icon check.."
+else
+    if zgrep "<icon type=\\'remote\\'>" "$XDG_CACHE_HOME/flatpak-builder-builddir/${1%.*}/files/share/app-info/xmls/${1%.*}.xml.gz" || test -f "$XDG_CACHE_HOME/flatpak-builder-builddir/${1%.*}/files/share/app-info/icons/flatpak/128x128/${1%.*}.png"
+    then
+        echo "128x128 icon check.. passed!"
+    else
+        echo "128x128 icon check.. failed!" >&2
+    fi
+fi
+
+if [ -e "$flathub_json" ] && python3 -c 'import sys, json; sys.exit(not json.load(sys.stdin).get("skip-appstream-check", False))' < "$flathub_json"
+then
+    echo "Skipping Appstream check"
+    echo "---"
+else
+    echo "Appstream check"
+    flatpak run org.freedesktop.appstream-glib validate "$XDG_CACHE_HOME/flatpak-builder-builddir/${1%.*}/files/share/appdata/${1%.*}.appdata.xml"
+    echo "---"
+fi
+
